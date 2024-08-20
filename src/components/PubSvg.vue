c0fba4fcd7f7f5466905a4ba874899b953dbe9b9
bingmax <bingmaxx@163.com>
2024-08-20 21:20:54 +0800

    feat: add package vite-svg-loader & components PubSvg

---
+++ src/components/PubSvg.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineAsyncComponent } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: 'currentColor',
+  },
+  size: {
+    type: [String, Number],
+    default: '24',
+  }
+});
+
+const ComponentSvg = defineAsyncComponent(() => import(`@/assets/svg/${props.name}.svg`))
+</script>
+
+<template>
+  <ComponentSvg :fill="color" :width="size" :height="size" />
+</template>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PubSvg from '@/components/PubSvg.vue'
 
 const app = createApp(App)
+
+app.component('PubSvg', PubSvg)
 
 app.use(router)
 
