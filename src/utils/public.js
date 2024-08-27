143a2282e1a3a596913330c99a3f5693a934329e
bingmax <bingmaxx@163.com>
2024-08-27 22:46:50 +0800

    feat: add component UnitTools

--- src/utils/public.js
+++ src/utils/public.js
@@ -5,3 +5,28 @@ export const colors = {
   warning: '#fa9d3f',
   danger: '#ff3c3c'
 }
+
+export const fabricPub = {
+  /**
+   * 功能列表
+   * immediate: 为 true，点击 tool 时不设置 tool_key
+   * draw: 是否有绘制模式, 为 true，点击空白出不退出 tool
+   * position: 为 true，图层选中时画布允许浮动
+   */
+  toolList: [
+    {
+      key: 'tools_img_add',
+      value: '加图',
+      immediate: true,
+      draw: false,
+      position: false
+    },
+    {
+      key: 'tools_text_add',
+      value: '文字',
+      immediate: true,
+      draw: false,
+      position: true
+    }
+  ]
+}
---
+++ src/views/editor/components/UnitTools.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { fabricPub } from '@/utils/public.js'
+
+const LIST = fabricPub.toolList
+
+const emit = defineEmits(['click'])
+
+function handle(key) {
+  emit('click', key)
+}
+</script>
+
+
+<template>
+  <div class="unit-tools">
+    <div class="box" v-for="item in LIST" :key="item.key" @click="handle(item.key)">
+      <PubSvg :name="item.key" color="white" size="1.25rem"></PubSvg>
+      <p>{{ item.value }}</p>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.unit-tools {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.box {
+  margin: 1.25rem 0.5rem 0 0.5rem;
+  min-width: calc(var(--fs-xs) * 4 + 0.125rem);
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.box p {
+  margin-top: 0.625rem;
+  color: var(--fc-3);
+  font-size: var(--fs-xs);
+}
+</style>
