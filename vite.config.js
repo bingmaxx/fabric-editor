55fae62864fe9cb8cff2380a2cd1d58600ce014e
bingmax <bingmaxx@163.com>
2024-10-10 21:05:47 +0800

    feat: add package vite-bundle-analyzer

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { analyzer } from 'vite-bundle-analyzer'
 // import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/fabric-editor/',
-  plugins: [vue(), svgLoader()],
+  plugins: [
+    vue(),
+    svgLoader(),
+    analyzer({
+      defaultSizes: 'parsed',
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
