e5624e33657ffc47903333c598fa592e418f4ad6
bingmax <bingmaxx@163.com>
2024-10-10 21:07:50 +0800

    feat: rm package vite-plugin-vue-devtools

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { analyzer } from 'vite-bundle-analyzer'
-// import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
-// https://vitejs.dev/config/
+
 export default defineConfig({
   base: '/fabric-editor/',
   plugins: [
